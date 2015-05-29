d1586f226916aa463638730fb0a91dd02b70f517
marieh <marieherelier@gmail.com>
2015-05-29 11:51:36 +0200

    modification route: nouvel article => accueil

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
    		@article.save
-  		redirect_to @article
+  		redirect_to articles_path
 	end
 	def show
 		@article = Article.find(params[:id])
