d61e70306efcb76d20dd681972e98e1c652a54fb
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-01 10:53:32 +0200

    message d'erreur pour les titres et sous-titres si format trop court ou trop long

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
-	validates :day, :month, :year, :title, :subtitle, :content, presence: true
+	validates :day, :month, :year,:content, presence: true
+	validates :title, presence: true,
+					length: { minimum: 12, maximum: 30, too_short: ": Votre titre est trop court", too_long: "Votre titre est trop long"}
+	validates :subtitle, presence: true,
+					length: { minimum: 12, too_short: "Votre sous-titre est trop court" }
 end
+
