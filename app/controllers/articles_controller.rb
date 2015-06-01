82a008ee881d5fd26ec479c6645d6ad78cb575af
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-01 09:57:35 +0200

    changement de routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,15 @@ class ArticlesController < ApplicationController
     	@articles = Article.all
   	end
 	def new
+    @article = Article.new
 	end
 	def create
 		@article = Article.new(article_params)
-   		@article.save
-  		redirect_to articles_path
+   		if @article.save
+        redirect_to articles_path
+      else
+      render 'new'
+      end
 	end
 	def show
 		@article = Article.find(params[:id])
