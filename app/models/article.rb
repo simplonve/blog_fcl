2a64079eeb5b7dd1ad79465c9aaaf642f0840d17
thibaultdescamps <descampsthibault@yahoo.fr>
2015-06-03 13:39:30 +0200

    lien fcl s'ouvre dans une nouvelle fenêtre / pas de max aux articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :day, :month, :year,:content, presence: true
 	validates :title, presence: true,
-					length: { minimum: 12, maximum: 30, too_short: ": Votre titre est trop court", too_long: "Votre titre est trop long"}
+					length: { minimum: 12, too_short: ": Votre titre est trop court"}
 	validates :subtitle, presence: true,
 					length: { minimum: 12, too_short: "Votre sous-titre est trop court" }
 end
