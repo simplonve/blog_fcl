b0991ee303f61d81a5d3f61094ab228e0c6e9b95
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-03 09:49:36 +0200

    ajout des commentaires

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+	has_many :comments
 	validates :day, :month, :year,:content, presence: true
 	validates :title, presence: true,
 					length: { minimum: 12, maximum: 30, too_short: ": Votre titre est trop court", too_long: "Votre titre est trop long"}
