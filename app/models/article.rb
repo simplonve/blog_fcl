f169c270884ca3e7bc978afa3b04eed47b808c60
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-03 09:59:35 +0200

    suppression commentaire liés aux articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :day, :month, :year,:content, presence: true
 	validates :title, presence: true,
 					length: { minimum: 12, maximum: 30, too_short: ": Votre titre est trop court", too_long: "Votre titre est trop long"}
