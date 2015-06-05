e2580a641fb7e31fe4e33f1bee9610da5492b309
marieh <marieherelier@gmail.com>
2015-06-05 16:46:12 +0200

    edit et new responsive/suppression exemple

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/example(.:format)' => 'application#example'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
